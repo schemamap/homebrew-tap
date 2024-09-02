d319365287b1c9b49a3e81dcecef9f149963f86b
Krisztian Szabo <thenonameguy24@gmail.com>
2024-09-02 16:58:30 +0200

    Distribute binaries instead of building from source

--- Formula/schemamap.rb
+++ Formula/schemamap.rb
@@ -1,29 +1,42 @@
 class Schemamap < Formula
   desc "Instant batch data import for Postgres"
   homepage "https://github.com/schemamap/schemamap"
-  url "https://github.com/schemamap/schemamap/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "422293dcd81f8f167622f90309282f36d3ddeb4e6ece7ecd8b4cd3abb97c4fe9"
+  version "0.3.0"
   license "MIT"
-  head "https://github.com/schemamap/schemamap.git", branch: "main"
 
   livecheck do
     url :stable
     strategy :github_latest
   end
 
-  depends_on "rust" => :build
+  on_macos do
+    on_intel do
+      url "https://github.com/schemamap/schemamap/releases/download/v0.3.0/schemamap-x86_64-apple-darwin.zip"
+      sha256 "533de7375c6e4f2c8e43b7841df8518ff3e362aa61e09ed45bf6265b95b81e70"
 
-  on_linux do
-    depends_on "openssl@3"
+      def install
+        bin.install "schemamap"
+      end
+    end
+
+    on_arm do
+      url "https://github.com/schemamap/schemamap/releases/download/v0.3.0/schemamap-aarch64-apple-darwin.zip"
+      sha256 "8ee2b6b88240f73a0569a0c1687b80ccb0f6f45caed4cb1b631012004063ab25"
+
+      def install
+        bin.install "schemamap"
+      end
+    end
   end
 
-  def install
-    # Ensure that the `openssl` crate picks up the intended library.
-    # https://crates.io/crates/openssl#manual-configuration
-    ENV["OPENSSL_DIR"] = Formula["openssl@3"].opt_prefix if OS.linux?
+  on_linux do
+    on_intel do
+      url "https://github.com/schemamap/schemamap/releases/download/v0.3.0/schemamap-x86_64-unknown-linux-gnu.zip"
+      sha256 "c62e00b6fc6f504494e201e9ecfadd5769608cf86d71787ece0fa8a50172b709"
 
-    cd "rust" do
-      system "cargo", "install", *std_cargo_args
+      def install
+        bin.install "schemamap"
+      end
     end
   end
 
